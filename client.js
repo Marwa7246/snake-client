6bf3d34b5ed16d9a35bc78a38e9de26b19bdf080
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-29 23:54:01 +0000

    added canned messages

--- client.js
+++ client.js
@@ -4,30 +4,30 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
 
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-  })
+  });
   
   conn.on('connect', () => {
     conn.write('Name: MHR');
 
-  })
+  });
 
   conn.on('data', (data) => {
     console.log(`Server says: ${data}`);
   });
 
   return conn;
-}
+};
 
 module.exports = {
   connect,
-}
+};
--- input.js
+++ input.js
@@ -1,24 +1,22 @@
-const  client  = require("./client");
-
 /**
- * Setup User Interface 
+ * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
 let connection;
 
 const setupInput = function(conn) {
-  connection = conn;   
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  stdin.on('data', handleUserInput)
+  stdin.on('data', handleUserInput);
  
   return stdin;
-}
+};
 
-const handleUserInput = function(key){
+const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
@@ -34,8 +32,18 @@ const handleUserInput = function(key){
   if (key === 's') {
     connection.write('Move: down');
   }
-}
+
+  if (key === '1') {
+    connection.write('Say: Hello');
+  }
+  if (key === '2') {
+    connection.write('Say: Bye');
+  }
+  if (key === '3') {
+    connection.write('Say: Nice');
+  }
+};
 
 module.exports = {
   setupInput,
-}
+};
