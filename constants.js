5f5bc50ee0232fe5c4daf8505df340b543daf52d
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-30 00:55:41 +0000

    Added constants.js file

---
+++ constants.js
@@ -0,0 +1,12 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+const mapMessages = { 1: 'Hello', 2: 'Bye', 3:  'nice'};
+const movements = {'w': 'up', 'a': 'left', 'd': 'right', 's': 'down'};
+
+module.exports = {
+  IP,
+  PORT,
+  mapMessages,
+  movements
+};
--- input.js
+++ input.js
@@ -3,9 +3,12 @@
  * Specifically, so that we can handle user input via stdin
  */
 let connection;
-
+const { mapMessages, movements } = require('./constants');
+ 
 const setupInput = function(conn) {
   connection = conn;
+  
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -20,28 +23,14 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
-    connection.write('Move: up');
-  }
-  if (key === 'a') {
-    connection.write('Move: left');
-  }
-  if (key === 'd') {
-    connection.write('Move: right');
-  }
-  if (key === 's') {
-    connection.write('Move: down');
+  if (movements[key]) {
+    connection.write(`Move: ${movements[key]}`);
   }
 
-  if (key === '1') {
-    connection.write('Say: Hello');
+  if (mapMessages[key]) {
+    connection.write('Say: ' + mapMessages[key]);
   }
-  if (key === '2') {
-    connection.write('Say: Bye');
-  }
-  if (key === '3') {
-    connection.write('Say: Nice');
-  }
+
 };
 
 module.exports = {
